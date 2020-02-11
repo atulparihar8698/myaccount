a191d74085b61ece11c5dc711f6067ede39f8142
atulparihar8698 <60177987+atulparihar8698@users.noreply.github.com>
2020-02-11 14:35:03 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-kubectl apply -f deployment.yml
-kubectl apply -f service.yml
+kubectl apply -f C:/Program Files (x86)/Jenkins/workspace/myaccount1/deployment.yml
+kubectl apply -f C:/Program Files (x86)/Jenkins/workspace/myaccount1/service.yml
